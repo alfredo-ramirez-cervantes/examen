ed2796dc08463de49fa71311cd357c294e4b6813
José Alfredo Ramírez Cervantes <alfredo.ramirez@semarnat.gob.mx>
2025-01-29 17:59:43 -0600

    Examen Base de datos Alfredo Ramirez Cervantes

--- ramirezCervantes_backend/src/main/java/com/examen/ramirezCervantes_backend/crud/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.examen.ramirezCervantes_backend.crud.repository;
-
-import com.examen.ramirezCervantes_backend.crud.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
